5bc452381a9cb51b7d7b3a015521084f27d7118d
kunuit <cuongkun2512@gmail.com>
2021-03-16 11:08:54 +0700

    profileScreen

--- src/components/profile.components/InfoCard.js
+++ src/components/profile.components/InfoCard.js
@@ -4,23 +4,24 @@ import { Card } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { theme } from '../../common/theme';
 
-const InfoCard = () => {
+const InfoCard = (props) => {
+  const {nameIcon, name} = props
   return (
     <Card style={{elevation: 0}} onPress={() => console.log('to item')}>
       <View  style={styles.root}>
 
         <Icon
-          name='ios-basket-outline'
+          name={nameIcon}
           size={30}
           
-          color={theme.colors.primary}
+          color={theme.colors.notBlack}
         />
-        <Text style={styles.nameItem}>Giỏ Hàng</Text>
+        <Text style={styles.nameItem}>{name}</Text>
         <Icon
           name='chevron-forward'
           size={20}
           
-          color={theme.colors.primary}
+          color={theme.colors.notBlack}
           />
       </View>
     </Card>
@@ -39,7 +40,10 @@ const styles = StyleSheet.create({
   },
   nameItem: {
     flex: 1,
-    marginLeft: 20
+    marginLeft: 20,
+    fontFamily: 'gilroy-light',
+    fontSize: 17,
+    color: theme.colors.notBlack
   }
 })
 
--- src/containers/screens/Profile.js
+++ src/containers/screens/Profile.js
@@ -4,18 +4,26 @@ import { theme } from '../../common/theme';
 import InfoCard from '../../components/profile.components/InfoCard';
 import InfoProfile from '../../components/profile.components/InfoProfile';
 
+
 const Profile = () => {
+  const arrInfoCard = [
+    {nameIcon: 'basket-outline', name:'Orders'},
+    {nameIcon: 'newspaper-outline', name:'My Details'},
+    {nameIcon: 'ios-location-outline', name:'Delivery Address'},
+    {nameIcon: 'ios-card-outline', name:'Payment Methods'},
+    {nameIcon: 'ios-barcode-outline', name:'Promo Card'},
+    {nameIcon: 'notifications-outline', name:'Notifications'},
+    {nameIcon: 'ios-help-circle-outline', name:'Help'},
+    {nameIcon: 'ios-alert-circle-outline', name:'About'},
+  ]
   return (
     <View style={styles.root}>
       <InfoProfile/>
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
+      {arrInfoCard.map((e,i) => {
+        return (
+          <InfoCard nameIcon={e.nameIcon} name={e.name} key={i} />
+        )
+      })}
     </View>
   );
 }
